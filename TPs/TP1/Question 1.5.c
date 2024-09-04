8d043f54bbd128b23f36a3c9b822192896c757bb
Sacha Kefifemail <sacha.kefif@edu.ece.fr>
2024-09-04 16:55:00 +0200

    Ajout des fichiers pour la partie 1.5

---
+++ TPs/TP1/Question 1.5.c
@@ -0,0 +1,5 @@
+//
+// Created by sacha on 04/09/2024.
+//
+
+#include "Question 1.5.h"
---
+++ TPs/TP1/Question 1.5.h
@@ -0,0 +1,8 @@
+//
+// Created by sacha on 04/09/2024.
+//
+
+#ifndef TP_ING2_QUESTION_1_5_H
+#define TP_ING2_QUESTION_1_5_H
+
+#endif //TP_ING2_QUESTION_1_5_H
